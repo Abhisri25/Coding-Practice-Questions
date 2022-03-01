e94da170b0c22882682758cd8b779895a0acbe96
Abhisri25 <100703304+Abhisri25@users.noreply.github.com>
2022-03-01 23:12:26 +0530

    Add files via upload

---
+++ Input and Output (HackerRank).cpp
@@ -0,0 +1,18 @@
+//Read 3 numbers from stdin and print their sum to stdout - HackerRank
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers:"<<endl;
+    cin>>a>>b>>c;
+    cout<<a+b+c;
+    return 0;
+}
