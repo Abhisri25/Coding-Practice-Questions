5a18e0aafa115597cf3af3e141b798d2fdc63768
Abhisri25 <100703304+Abhisri25@users.noreply.github.com>
2022-03-02 23:52:00 +0530

    Add files via upload

---
+++ C basic.c
@@ -0,0 +1,14 @@
+//You have to print the character, ch , in the first line. Then print s in next line. In the last line print the sentence, sen.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("H\n");
+    printf("Hello\n");
+    printf("How are you?");
+    return 0;
+}
