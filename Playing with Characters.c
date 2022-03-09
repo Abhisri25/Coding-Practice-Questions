31040dc2c4ba361282dc46bc4d29f997640f1fbd
Abhisri25 <100703304+Abhisri25@users.noreply.github.com>
2022-03-09 21:06:22 +0530

    Add files via upload

---
+++ Playing with Characters.c
@@ -0,0 +1,16 @@
+// Playing With Characters - Hacker Rank
+
+#include <stdlib.h>
+#define MAX_LIMIT 40
+int main()
+{
+    char ch;
+    char s[MAX_LIMIT];
+    char sen[MAX_LIMIT];
+    scanf("%c",&ch);
+    scanf("%s",&s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    printf("%c\n%s\n%s",ch,s,sen);
+    return 0;
+}
