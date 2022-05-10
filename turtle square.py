3c42673a3b5f722674d37337a2c9ca3cae52a67e
Abhisri25 <100703304+Abhisri25@users.noreply.github.com>
2022-05-10 22:30:23 +0530

    Add files via upload

---
+++ turtle square.py
@@ -0,0 +1,21 @@
+import turtle
+t = turtle.Turtle()
+
+def square():
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+
+square()
+t.forward(100)
+square()
+t.left(90)
+t.forward(100)
+square()
+t.right(90)
+square()
